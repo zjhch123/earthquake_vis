2bf7a5776c82d7d9715a347c9b783da66c9e4432
zjhch123 <416836508@qq.com>
2019-12-17 21:32:08 +0800

    Update: add scroll logic

--- src/App.js
+++ src/App.js
@@ -1,2 +1,10 @@
 import '@css/style.scss'
 import '@js/index'
+
+const Fullpage = window.fullpage
+
+new Fullpage('#app_start', {
+  autoScrolling: true,
+  scrollHorizontally: true,
+  licenseKey: 'dddddddd-dddddddd-dddddddd-dddddddd',
+})
